c9613d992b5c9ef24c59f861a2ae22c04f4d444c
gaetaegoo <gaetaegoo@gmail.com>
2022-03-17 10:23:20 +0900

    220317-10:22-1이 될 떄까지(방법 추가)

--- greedy(탐욕법)/greedy(예제3-4).py
+++ greedy(탐욕법)/greedy(예제3-4).py
@@ -13,6 +13,19 @@ input = sys.stdin.readline
 n, k = map(int, input().split())
 result = 0
 
+while n != 1:
+    if n % k != 0:
+        n = n - 1
+        result += 1
+
+    else:
+        n = n / k
+        result += 1
+
+print(result)
+
+# 두 번째 방법 -------------------------------------------------------------
+
 # n이 k 이상이라면 k로 계속 나누기
 while n >= k:
     while n % k != 0:   # n이 k로 나누어 떨어지지 않으면 n에서 1씩 빼기
