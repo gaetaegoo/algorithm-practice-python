6de9965fa1bee29b80e44a577e89e74384b602e2
gaetaegoo <gaetaegoo@gmail.com>
2022-03-17 10:01:31 +0900

    220317-10:01-1이 될 떄까지

--- greedy(탐욕법)/greedy(예제3-2).py
+++ greedy(탐욕법)/greedy(예제3-2).py
@@ -1,4 +1,10 @@
 # 실전 문제 - 큰 수의 법칙
+'''
+다양한 수로 이루어진 배열이 있을 때
+주어진 수들을 M번 더하여 가장 큰 수를 만들어라
+단 배열의 특정한 인덱스(번호)에 해당하는 수가
+연속해서 K번을 초과하여 더해질 수 없다
+'''
 
 import sys
 input = sys.stdin.readline
@@ -19,3 +25,29 @@ result += (m - count) * data [n - 2]    # 두 번째로 큰 수 더하기
 
 print(result)
 
+# 두번 째 방법 ---------------------------------------------------------------
+
+import sys
+input = sys.stdin.readline
+
+n, m, k = map(int, input().split())     # 공백으로 구분하여 입력 받기
+data = list(map(int, input().split()))  # 공백으로 구분하여 리스트에 담기
+
+data.sort() # 입력 받은 수 정렬
+first = data[n - 1] # 가장 큰 수
+second = data[n - 2]    # 두 번째로 큰 수
+
+result = 0
+
+while True:
+    for i in range(k):  # 가장 큰 수를 k번 더하기
+        if m == 0:  # m이 0이라면 반복문 탈출
+            break
+        result += first
+        m -= 1  # 더할 때 마다 1씩 빼기
+    if m == 0:  # m이 0이라면 반복문 탈출
+        break
+    result += second # 두 번째로 큰 수를 한 번 더하기
+    m -= 1 # 더할 때마다 1씩 빼기
+
+print(result)
---
+++ greedy(탐욕법)/greedy(예제3-4).py
@@ -0,0 +1,28 @@
+# 1이 될 때까지
+'''
+어떠한 수 N이 1이 될 때까지 다음의 두 과정 중
+하나를 반복적으로 선택하여 수행하려고 한다
+단, 두 번째 연산은 N이 K로 나누어떨어질 때만 선택할 수 있다
+1. N에서 1을 뺀다
+2. N을 K로 나눈다
+'''
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+result = 0
+
+# n이 k 이상이라면 k로 계속 나누기
+while n >= k:
+    while n % k != 0:   # n이 k로 나누어 떨어지지 않으면 n에서 1씩 빼기
+        n -= 1
+        result += 1
+    n //= k
+    result += 1
+
+while n < 1:    # 마지막으로 남은 수에 대하여 1씩 빼기
+    n -= 1
+    result +=1
+
+print(result)
