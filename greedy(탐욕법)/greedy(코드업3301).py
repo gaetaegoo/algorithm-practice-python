6e98a63a8db7d8844ddafb5f0fc5834a31367b60
unknown <gaetaegoo@gmail.com>
2022-04-13 16:37:54 +0900

    220413-16:37-거스름돈

---
+++ greedy(탐욕법)/greedy(코드업3301).py
@@ -0,0 +1,29 @@
+# https://codeup.kr/problem.php?id=3301
+# 3301 : 거스름돈
+
+# 방법 1 ----------------------------------------------
+n = int(input())
+
+num = 0
+change = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+
+for i in change:    # if절과 break 활용
+    if n >= i:
+        sum += n // i
+        n %= i
+        if n <= 0:
+            break
+
+print(num)
+
+# 방법 2 ----------------------------------------------
+n = int(input())
+
+num = 0
+change = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+
+for m in change:    # 단순히 for문만 돌려도 됨
+    num += n // m
+    n = n % m
+
+print(num)
